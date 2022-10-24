1478ff51b5b2b514aeb84a5f09639b7bc998b18d
Andrew Stanecek <andrew.stanecek@keysight.com>
2022-10-24 15:50:40 -0600

    dockerized

--- firstapp/settings/dev.py
+++ firstapp/settings/dev.py
@@ -13,28 +13,32 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'USER': 'root',
         'PASSWORD': 'bayside1'
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
 
 # if DEBUG:
 #     MIDDLEWARE += ['silk.middleware.SilkyMiddleware']
